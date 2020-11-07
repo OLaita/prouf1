137bfc0ddc3b73303e2ef5eb1c07f55f3fe642c3
OLaita <oscarlaitaro@gmail.com>
2020-11-07 20:47:22 +0100

    comentario

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
     $dbhost = "127.0.0.1";
-    $dbname = "prouf1";
-    $dbuser = "prouf1";
+    $dbname = "LaOs_prouf1";
+    $dbuser = "LaOs_prouf1";
     $dbpass = "oscar7";
     $dsn = 'mysql:host='.$dbhost.';dbname='.$dbname;
